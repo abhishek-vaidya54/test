e9194d6fe22a53d269ece5841e1430ad7d908b11
luke lombardi <luke@strongarmtech.com>
2018-08-27 17:53:16 -0400

    fix migration history

---
+++ pipeline/alembic/versions/acfc7b5ba512_adding_two_columns_to_warehouse_update_.py
@@ -0,0 +1,25 @@
+"""adding two columns to warehouse, update/show engagement
+
+Revision ID: acfc7b5ba512
+Revises: 32a4cb053b94
+Create Date: 2018-08-22 17:10:29.610823
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'acfc7b5ba512'
+down_revision = '32a4cb053b94'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column('warehouse', sa.Column('show_engagement', sa.Boolean(), nullable=False, server_default=sa.false()))
+    op.add_column('warehouse', sa.Column('update_engagement', sa.Boolean(), nullable=False, server_default=sa.true()))
+
+def downgrade():
+    op.drop_column('warehouse', 'show_engagement')
+    op.drop_column('warehouse', 'update_engagement')
