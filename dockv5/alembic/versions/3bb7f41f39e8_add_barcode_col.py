4eef6dc2f60a0a9bc958ad0fc318bd3f8725cef6
Mercer Borris <mercer@strongarmtech.com>
2018-11-06 12:26:52 -0500

    added imei column to config table

---
+++ dockv5/alembic/versions/3bb7f41f39e8_add_barcode_col.py
@@ -0,0 +1,24 @@
+"""add barcode col
+
+Revision ID: 3bb7f41f39e8
+Revises: f168e0982cff
+Create Date: 2018-11-01 12:03:54.501298
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3bb7f41f39e8'
+down_revision = 'f168e0982cff'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ dockv5/alembic/versions/f168e0982cff_add_imei_col.py
@@ -0,0 +1,25 @@
+"""add imei col
+
+Revision ID: f168e0982cff
+Revises: e79ea302e61f
+Create Date: 2018-10-31 12:36:07.141055
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f168e0982cff'
+down_revision = 'e79ea302e61f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute("""
+      ALTER TABLE dockv5.config ADD COLUMN `dock_imei` VARCHAR(45) NULL DEFAULT NULL AFTER `dock_id` 
+    """)
+
+def downgrade():
+    op.drop_column('config', 'dock_imei')
