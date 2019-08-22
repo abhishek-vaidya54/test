c91c559381aec11013a06c8d9dd04521fda7c586
vikashStr <vikash@strongarmtech.com>
2019-08-22 15:06:25 -0400

    changing to lower case Default

--- pipeline/alembic/versions/2efae14f67f9_metadata_table.py
+++ pipeline/alembic/versions/2efae14f67f9_metadata_table.py
@@ -23,12 +23,12 @@ def upgrade():
         sa.Column('processed_file_id', sa.Integer(), nullable=False),
         sa.Column('metadata_type', sa.String(length=255), nullable=False),
         sa.Column('value', sa.String(length=255), nullable=False),
-        sa.Column('exclude', sa.Boolean(), Default= 0, nullable=False),
+        sa.Column('exclude', sa.Boolean(), default= 0, nullable=False),
         sa.Column('db_created_at', sa.DateTime, default=datetime.datetime.utcnow, nullable=False),
-        sa.Column('notes', sa.JSON(), Nullable=True),
+        sa.Column('notes', sa.JSON(), nullable=True),
         sa.PrimaryKeyConstraint('id'),
         sa.ForeignKeyConstraint(['processed_file_id'], ['processed_file.id'], name= 'activity_ibfk_3')
     )
-    
+
 def downgrade():
     op.drop_table('metadata')
