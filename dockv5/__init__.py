4d90ba11466db79282abdef3417ff55a39ac2501
Vincent Turnier <vincent@strongarmtech.com>
2019-09-13 11:53:22 -0400

    Updated Dock_phase_table

--- dockv5/__init__.py
+++ dockv5/__init__.py
@@ -1,32 +1,32 @@
-import os
+# import os
 
-from copy import deepcopy
+# from copy import deepcopy
 
-from sqlalchemy.sql.functions import GenericFunction
-from sqlalchemy.types import DateTime
+# from sqlalchemy.sql.functions import GenericFunction
+# from sqlalchemy.types import DateTime
 
-from sqlalchemy.exc import DBAPIError
-from sqlalchemy_wrapper import SQLAlchemy
-
-
-if RUNTIME_ENV == 'LAMBDA':
-    from config import Config
-else:
-    from pipeline.config import Config
-
-db = SQLAlchemy(Config.DB_URL)
-UTC_TIMEZONE = 'UTC'
+# from sqlalchemy.exc import DBAPIError
+# from sqlalchemy_wrapper import SQLAlchemy
 
 
-def commit_or_rollback(session):
-    try:
-        session.commit()
-    except DBAPIError:
-        session.rollback()
-        raise
+# if RUNTIME_ENV == 'LAMBDA':
+#     from config import Config
+# else:
+#     from pipeline.config import Config
 
-class convert_tz(GenericFunction):
-    type = DateTime
+# db = SQLAlchemy(Config.URL)
+# UTC_TIMEZONE = 'UTC'
 
 
-from .config import Config
+# def commit_or_rollback(session):
+#     try:
+#         session.commit()
+#     except DBAPIError:
+#         session.rollback()
+#         raise
+
+# class convert_tz(GenericFunction):
+#     type = DateTime
+
+
+# from .config import Config
