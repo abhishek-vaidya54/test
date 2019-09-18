a8e2317396233c5886753b2febe26c753f95a255
Vincent Turnier <vincent@strongarmtech.com>
2019-09-18 16:48:14 -0400

    industrial_athlete_common_base_integration

---
+++ pipeline/main.py
@@ -0,0 +1,10 @@
+from database_models.pipeline.base import Base
+
+from database_models.pipeline.client import Client
+from database_models.pipeline.warehouse import Warehouse
+from database_models.pipeline.job_function import JobFunction
+from database_models.pipeline.shifts import Shifts
+from database_models.pipeline.industrial_athlete import IndustrialAthlete
+
+
+IndustrialAthlete = IndustrialAthlete
