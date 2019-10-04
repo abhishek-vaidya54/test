cce58479797e2eb26c3579e8d25f99c30f72c6a4
Vincent Turnier <vincent@strongarmtech.com>
2019-10-04 16:05:40 -0400

    added base test for dockv5 & dockv5_eventslog

---
+++ src/sat_orm/test/unit/dockv5/test_dockv5_base.py
@@ -0,0 +1,23 @@
+# Standard Library Imports
+import os
+
+# Third Party Imports
+import pytest
+
+# Local Application Imports
+from sat_orm.dockv5_orm import dockv5_base
+
+@pytest.mark.orm_base
+def test_get_connection_string_is_none():
+    with pytest.raises(Exception) as exc_info:
+        assert dockv5_base.get_connection_string(0)
+    assert 'cannot be none' in str(exc_info.value)
+
+@pytest.mark.orm_base
+def test_connection_string():
+    assert dockv5_base.Base
+    assert dockv5_base.engine
+    assert dockv5_base.connection.begin()
+    assert dockv5_base.session
+    assert dockv5_base.session.close() == None
+    assert dockv5_base.connection.close() == None
