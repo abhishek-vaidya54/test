68b9991199fd4ffe99dd18d25ab4975496e562b8
Vincent Turnier <vincent@strongarmtech.com>
2019-10-04 17:17:07 -0400

    cleaning folders & and modified setup version to 0.0.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sat_orm',
-    version='0.0.5',
+    version='0.0.7',
     packages= find_packages('src'),
     package_dir={'':'src'},
     install_requires=[
