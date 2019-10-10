31df2b4593aaa397802ae6ce2c894b0527388b37
Vincent Turnier <vincent@strongarmtech.com>
2019-10-09 20:19:17 -0400

    config.dock_phase return current dock_phase

--- src/sat_orm/dockv5_orm/dock_phase.py
+++ src/sat_orm/dockv5_orm/dock_phase.py
@@ -50,7 +50,7 @@ class DockPhase(Base):
     phase = Column(Enum('PREP','DEMO','INFIELD','MAINTENANCE','UNUSED','RETIRED'),nullable=False,default='PREP')
     deployment_stage = Column(String(20), nullable=False)
 
-    config = relationship('Config',back_populates='dock_phases')
+    config = relationship('Config',back_populates='dock_phase')
 
     @validates('dock_id')
     def validate_dock_id(self,key,dock_id):
