53af2ff4fe0b0c60e20dfeb91ea9433fc8335728
Reuben <reuben@strongarmtech.com>
2020-01-30 15:16:50 -0500

    remove dock_phase

--- src/sat_orm/dockv5_factories.py
+++ src/sat_orm/dockv5_factories.py
@@ -23,7 +23,6 @@ DESCRIPTION:
             | Dockv5 Tables |
             +---------------+
             | config        |
-            | dock_phase    |
             +---------------+
 
             **** Edit This File If tables are added or removed ****
@@ -37,7 +36,7 @@ import string
 import datetime
 
 
-from sat_orm.dockv5 import Config, DockPhase
+from sat_orm.dockv5 import Config
 
 def generate_dock_id():
     hex_letters = ['A','B','C','D','E','F']
@@ -54,28 +53,6 @@ class ConfigFactory(factory.alchemy.SQLAlchemyModelFactory):
     warehouse_id = factory.fuzzy.FuzzyInteger(low=10000,high=11000,step=1)
     deployment_stage = factory.fuzzy.FuzzyChoice(['dev','prod'])
     
-    @factory.post_generation
-    def dock_phases(self, create, extracted, **kwargs):
-        if extracted is None:
-            extracted = 0
-
-        make_dock_phase = getattr(DockPhaseFactory, 'create' if create else 'build')
-        self.dock_phases = [make_dock_phase(dock_id=self.dock_id) for i in range(extracted)]
-
-        if not create:
-            self._prefetched_objects_cache = {'dock_phases':self.dock_phases}
-
-
     class Meta:
         model = Config
-        sqlalchemy_session_persistence = 'commit'
-
-
-class DockPhaseFactory(factory.alchemy.SQLAlchemyModelFactory):
-    dock_id = factory.SubFactory(ConfigFactory)
-    timestamp = factory.LazyFunction(datetime.datetime.now)
-    phase = factory.fuzzy.FuzzyChoice(['DEPLOYED','NOT DEPLOYED','MAINTENANCE'])
-    
-    class Meta:
-        model = DockPhase
         sqlalchemy_session_persistence = 'commit'
