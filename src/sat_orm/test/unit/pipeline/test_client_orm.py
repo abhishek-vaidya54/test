7ed7b150603932d192890a8a095448c8f3a551bf
Reuben <reuben@strongarmtech.com>
2020-02-03 12:50:14 -0500

    update client model test case

--- src/sat_orm/test/unit/pipeline/test_client_orm.py
+++ src/sat_orm/test/unit/pipeline/test_client_orm.py
@@ -19,24 +19,10 @@ def test_client_validate_prefix():
     assert 'cannot be Null' in str(exc_info.value)
 
 @pytest.mark.input_validation
-def test_client_validate_guid():
-    ''' Validates client guid column'''
-    with pytest.raises(Exception) as exc_info:
-        assert Client(guid=None)
-    assert 'cannot be Null' in str(exc_info.value)
-
-@pytest.mark.input_validation
 def test_client_validate_enable_processing():
     ''' Validates client enable_processing column'''
     with pytest.raises(Exception) as exc_info:
         assert Client(enable_processing=None)
-    assert 'cannot be Null' in str(exc_info.value)
-
-@pytest.mark.input_validation
-def test_client_validate_dynamic_shift():
-    ''' Validates client dynamic_shift column'''
-    with pytest.raises(Exception) as exc_info:
-        assert Client(dynamic_shift=None)
     assert 'cannot be Null' in str(exc_info.value)
 
 @pytest.mark.test_return_type
