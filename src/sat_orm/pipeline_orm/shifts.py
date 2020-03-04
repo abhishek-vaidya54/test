e24506640fb19bfcb2e94ae6ce12aa1202e03783
Hashmat Ibrahimi <Hashmatibrahimi0711@gmail.com>
2020-03-04 15:05:26 -0500

    added needed changes to shifts.py

--- src/sat_orm/pipeline_orm/shifts.py
+++ src/sat_orm/pipeline_orm/shifts.py
@@ -17,7 +17,7 @@ CLASSIFICATION:
 import datetime
 
 # Third Party Imports
-from sqlalchemy import ForeignKey, Column, Integer, DateTime, Text, Time, String
+from sqlalchemy import ForeignKey, Column, Integer, DateTime, Text, String
 from sqlalchemy.orm import relationship, validates
 
 # Local Application Import
@@ -30,8 +30,8 @@ class Shifts(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     warehouse_id = Column(Integer,ForeignKey('warehouse.id'),nullable=False)
     name = Column(String(255), nullable=False)
-    shift_start = Column(Time, nullable=False)
-    shift_end = Column(Time, nullable=False)
+    shift_start = Column(DateTime, nullable=False)
+    shift_end = Column(DateTime, nullable=False)
     group_administrator = Column(String(255), nullable=False)
     description = Column(Text, nullable=True)
     color = Column(String(255), nullable=True)
