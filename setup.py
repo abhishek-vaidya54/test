532150bc31b4d99412db27b6306ee0ab0b6244b5
Hashmat Ibrahimi <Hashmatibrahimi0711@gmail.com>
2020-04-15 11:24:40 -0400

    updating database model for staging

--- setup.py
+++ setup.py
@@ -3,10 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='sat_orm',
     version='0.2.3',
-    packages= find_packages('src'),
-    package_dir={'':'src'},
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
     install_requires=[
-        'sqlalchemy','pymysql','pytest','factory-boy', 'smalluuid'
+        'sqlalchemy', 'pymysql', 'pytest', 'factory-boy', 'smalluuid'
     ]
 )
-
