18edd152019e356e7ccfe73770d007899e4cd514
Mukul Shakya <muklshakya123@gmail.com>
2020-07-27 20:53:12 +0530

    dock field migrations

--- dockv5/alembic/versions/97618bd2ecc2_add_new_dock_fields.py
+++ dockv5/alembic/versions/97618bd2ecc2_add_new_dock_fields.py
@@ -23,7 +23,7 @@ def upgrade():
             "client_id",
             sa.Integer(),
             sa.ForeignKey("pipeline.client.id"),
-            nullable=False,
+            nullable=True,
         ),
     )
     op.add_column(
@@ -32,7 +32,7 @@ def upgrade():
             "warehouse_id",
             sa.Integer(),
             sa.ForeignKey("pipeline.warehouse.id"),
-            nullable=False,
+            nullable=True,
         ),
     )
     op.add_column(
