c5e74154bb24e9b4cde60b356539398e78c1909f
Mukul Shakya <muklshakya123@gmail.com>
2020-08-11 16:18:09 +0530

    added client_id in athlete_upload_status

--- src/sat_orm/pipeline_orm/athlete_upload_status.py
+++ src/sat_orm/pipeline_orm/athlete_upload_status.py
@@ -8,36 +8,33 @@ from sqlalchemy import ForeignKey, true, false
 from sqlalchemy.dialects.mysql import INTEGER
 
 from itsdangerous import TimedJSONWebSignatureSerializer
-from sqlalchemy import ForeignKey, Column, String, Integer, DateTime , desc
+from sqlalchemy import ForeignKey, Column, String, Integer, DateTime, desc
 from sqlalchemy.orm import relationship, validates
 
 logger = logging.getLogger(__name__)
 from sat_orm.pipeline_orm.pipeline_base import Base
 
+
 class AthleteUploadStatus(Base):
-    __tablename__ = 'athlete_upload_status'
+    __tablename__ = "athlete_upload_status"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     username = Column(String(255), nullable=False)
     processed = Column(Integer, nullable=False)
     total = Column(Integer, nullable=False)
-    db_created_at = Column(
-        DateTime,
-        default=datetime.datetime.utcnow,
-        nullable=False
-    )
+    client_id = Column(Integer, nullable=False)
+    db_created_at = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
     db_updated_at = Column(
         DateTime,
         default=datetime.datetime.utcnow,
         onupdate=datetime.datetime.utcnow,
-        nullable=False
+        nullable=False,
     )
-
 
     def as_dict(self):
         return {
-            'processed': getattr(self, 'processed'),
-            'total': getattr(self, 'total'),
+            "processed": getattr(self, "processed"),
+            "total": getattr(self, "total"),
         }
 
     def __repr__(self):
