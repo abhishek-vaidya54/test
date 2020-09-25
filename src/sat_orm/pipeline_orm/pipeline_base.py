715c97634293db9da3a281d8259d85fa22cbceca
Mukul Shakya <muklshakya123@gmail.com>
2020-09-25 16:54:20 +0530

    session referencing

--- src/sat_orm/pipeline_orm/pipeline_base.py
+++ src/sat_orm/pipeline_orm/pipeline_base.py
@@ -17,11 +17,31 @@ CLASSIFICATION:
 import os
 
 # Third Party Library Imports
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 connection_string = os.environ.get("PIPELINE_CONNECTION_STRING", 0)
+
+
+def strong_reference_session(session):
+    @event.listens_for(session, "pending_to_persistent")
+    @event.listens_for(session, "deleted_to_persistent")
+    @event.listens_for(session, "detached_to_persistent")
+    @event.listens_for(session, "loaded_as_persistent")
+    def strong_ref_object(sess, instance):
+        if "refs" not in sess.info:
+            sess.info["refs"] = refs = set()
+        else:
+            refs = sess.info["refs"]
+
+        refs.add(instance)
+
+    @event.listens_for(session, "persistent_to_detached")
+    @event.listens_for(session, "persistent_to_deleted")
+    @event.listens_for(session, "persistent_to_transient")
+    def deref_object(sess, instance):
+        sess.info["refs"].discard(instance)
 
 
 def get_connection_string(connection_string):
@@ -37,4 +57,5 @@ engine = create_engine(get_connection_string(connection_string))
 connection = engine.connect()
 Session = sessionmaker(bind=connection)
 session = Session()
+strong_reference_session(session)
 Base = declarative_base()
