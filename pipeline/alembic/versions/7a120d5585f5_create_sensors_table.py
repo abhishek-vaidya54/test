f1c2d3dd8e4dc5636afcadf8397c7c48739bb7fd
Mukul Shakya <muklshakya123@gmail.com>
2020-09-29 12:11:44 +0530

    Sensor model updates

---
+++ pipeline/alembic/versions/7a120d5585f5_create_sensors_table.py
@@ -0,0 +1,45 @@
+"""create sensors table
+
+Revision ID: 7a120d5585f5
+Revises: b0d0b3c39ada
+Create Date: 2020-09-28 16:49:24.093418
+
+"""
+import datetime
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "7a120d5585f5"
+down_revision = "b0d0b3c39ada"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        "sensors",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("serial_number", sa.String(length=45), nullable=False),
+        sa.Column("sensor_id", sa.String(length=45), nullable=True),
+        sa.Column("stiction_flagged", sa.Boolean(), default=False, nullable=False),
+        sa.Column("decommissioned", sa.Boolean(), default=False, nullable=False),
+        sa.Column(
+            "db_created_at",
+            sa.DateTime,
+            default=datetime.datetime.utcnow,
+            nullable=False,
+        ),
+        sa.Column(
+            "db_modified_at",
+            sa.DateTime,
+            default=datetime.datetime.utcnow,
+            nullable=False,
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+def downgrade():
+    op.drop_table("sensors")
