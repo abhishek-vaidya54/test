d08fe9c63553ec936bf15918454e9e3ff62500a1
Mukul Shakya <muklshakya123@gmail.com>
2020-09-29 19:17:20 +0530

    Removed the except block from pipeline base

--- src/sat_orm/pipeline_orm/pipeline_base.py
+++ src/sat_orm/pipeline_orm/pipeline_base.py
@@ -16,32 +16,21 @@ CLASSIFICATION:
 # Standard Library Imports
 import os
 
+from ddtrace import patch
+
+patch(sqlalchemy=True)
+
 # Third Party Library Imports
-from sqlalchemy import create_engine, event
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import session, scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-connection_string = os.environ.get("PIPELINE_CONNECTION_STRING", 0)
+from contextlib import contextmanager
 
+import logging
 
-def strong_reference_session(session):
-    @event.listens_for(session, "pending_to_persistent")
-    @event.listens_for(session, "deleted_to_persistent")
-    @event.listens_for(session, "detached_to_persistent")
-    @event.listens_for(session, "loaded_as_persistent")
-    def strong_ref_object(sess, instance):
-        if "refs" not in sess.info:
-            sess.info["refs"] = refs = set()
-        else:
-            refs = sess.info["refs"]
-
-        refs.add(instance)
-
-    @event.listens_for(session, "persistent_to_detached")
-    @event.listens_for(session, "persistent_to_deleted")
-    @event.listens_for(session, "persistent_to_transient")
-    def deref_object(sess, instance):
-        sess.info["refs"].discard(instance)
+LOGGER = logging.getLogger()
+logging.basicConfig()
 
 
 def get_connection_string(connection_string):
@@ -53,9 +42,35 @@ def get_connection_string(connection_string):
         )
 
 
-engine = create_engine(get_connection_string(connection_string))
-connection = engine.connect()
-Session = sessionmaker(bind=connection)
-session = Session()
-strong_reference_session(session)
+# engine = create_engine(get_connection_string(connection_string), pool_pre_ping=True)
+# connection = engine.connect()
+# session = scoped_session(sessionmaker(bind=connection))
+# https://gist.github.com/brianz/2d7001bf1b0bafa9379303aa2da4cdeb
+
+
+@contextmanager
+def get_session():
+    connection_string = os.environ.get("PIPELINE_CONNECTION_STRING", 0)
+
+    engine = create_engine(get_connection_string(connection_string), pool_pre_ping=True)
+    connection = engine.connect()
+    # https://stackoverflow.com/questions/12223335/sqlalchemy-creating-vs-reusing-a-session?rq=1
+    db_session = scoped_session(sessionmaker(bind=connection))
+
+    # try:
+    yield db_session
+
+    """
+        Reason for change - 
+        errors in handler is been getting caught in the below except block 
+        as the handler returns Exception for AWS to parse it as Bad Request.
+    """
+    # except Exception as error:
+    #     LOGGER.exception("%s %s %s", "ERROR", "get_session()", error)
+    #     return Exception(error)
+    # finally:
+    db_session.remove()
+    connection.close()
+
+
 Base = declarative_base()
