4394355e66e09f1c1dfdcbe58bb634ce52186d64
Mukul Shakya <muklshakya123@gmail.com>
2020-10-06 19:52:30 +0530

    create external_id and warehouse_id index in industrial_athlete

---
+++ pipeline/alembic/versions/f2d0e56105a8_create_external_id_and_warehouse_id_.py
@@ -0,0 +1,24 @@
+"""create external_id and warehouse_id index in industrial_athlete
+
+Revision ID: f2d0e56105a8
+Revises: 7a120d5585f5
+Create Date: 2020-10-06 18:54:28.570868
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "f2d0e56105a8"
+down_revision = "7a120d5585f5"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute("ALTER TABLE industrial_athlete ADD UNIQUE (external_id, warehouse_id);")
+
+
+def downgrade():
+    op.execute("DROP INDEX `external_id` ON industrial_athlete")
