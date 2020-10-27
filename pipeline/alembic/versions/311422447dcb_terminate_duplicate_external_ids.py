52dcf8786e73e13f44f95d363948d148e5c23f1d
Joe Siefers <joe@strongarmtech.com>
2020-10-26 22:35:19 -0400

    Reorder migrations to correct inactive athlete data.

--- pipeline/alembic/versions/311422447dcb_terminate_duplicate_external_ids.py
+++ pipeline/alembic/versions/311422447dcb_terminate_duplicate_external_ids.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "311422447dcb"
-down_revision = "75c576d02328"
+down_revision = "7a120d5585f5"
 branch_labels = None
 depends_on = None
 
--- pipeline/alembic/versions/f2d0e56105a8_create_external_id_and_warehouse_id_.py
+++ pipeline/alembic/versions/f2d0e56105a8_create_external_id_and_warehouse_id_.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "f2d0e56105a8"
-down_revision = "7a120d5585f5"
+down_revision = "311422447dcb"
 branch_labels = None
 depends_on = None
 
