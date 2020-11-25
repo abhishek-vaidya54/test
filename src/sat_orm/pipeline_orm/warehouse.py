88fafdc2e61307f9d60a4ca48cf4fbe0b77d979b
DipdlavSingh <diplav@strongarmtech.com>
2020-11-25 17:19:50 +0530

    add check_error_and_return function

--- src/sat_orm/pipeline_orm/warehouse.py
+++ src/sat_orm/pipeline_orm/warehouse.py
@@ -214,8 +214,4 @@ def validate_before_insert(mapper, connection, target):
                 errors.append(utils.build_error(key, key + constants.INVALID_LAT_LONG_DIRECTION_MESSAGE))
 
 
-    if len(errors) > 0:
-        error_response = copy.deepcopy(constants.ERROR)
-        error_response["message"] = constants.INVALID_PARAMS_MESSAGE
-        error_response["errors"] = errors
-        raise Exception(json.dumps(error_response))
+    utils.check_errors_and_return(errors)
