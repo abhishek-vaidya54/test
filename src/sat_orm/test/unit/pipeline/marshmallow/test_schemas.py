02ea53d8785d6cbd9ef188f3c29550bf8cb46753
DipdlavSingh <diplav@strongarmtech.com>
2020-12-03 17:46:37 +0530

    add warehoues scemas unit test

--- src/sat_orm/test/unit/pipeline/marshmallow/test_schemas.py
+++ src/sat_orm/test/unit/pipeline/marshmallow/test_schemas.py
@@ -1,4 +1,4 @@
-from sat_orm.pipeline_orm.marshmallow.schemas import SettingSchema
+from sat_orm.pipeline_orm.marshmallow.schemas import *
 
 def test_setting_schema_exist(get_setting_type_athlete):
     setting = get_setting_type_athlete
@@ -19,3 +19,26 @@ def test_setting_schema_does_not_exist(get_setting_type_athlete):
         ]
     result = SettingSchema(only=keys).dump(setting)
     assert "id" not in result
+
+def test_warehouse_schema_exist(get_warehouse_from_db):
+    warehouse = get_warehouse_from_db
+    keys = [
+            "id",
+            "client_id",
+            "name",
+            "location",
+        ]
+    result = WarehouseSchema(only=keys).dump(warehouse)
+    for key in keys:
+        assert result[key]
+
+def test_warehouse_schema_does_not_exist(get_warehouse_from_db):
+    warehouse = get_warehouse_from_db
+    keys = [
+            "client_id",
+            "name",
+            "location",
+        ]
+    result = WarehouseSchema(only=keys).dump(warehouse)
+    assert "id" not in result
+
