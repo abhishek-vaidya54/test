dfd2f9446992084197dd31fc484e2a3c74f4ebe4
Mukul Shakya <muklshakya123@gmail.com>
2021-01-05 13:00:26 +0530

    Added looker role

--- src/sat_orm/constants.py
+++ src/sat_orm/constants.py
@@ -222,6 +222,7 @@ RBAC_VALID_RESOURCES = (
     "users",
     "settings",
     "sensors",
+    "looker"
 )
 
 RBAC_VALID_ACTIONS = ("read", "write", "update", "delete")
@@ -252,7 +253,7 @@ POLICY_GROUPS_OBJ = "groups"
 POLICY_EXTERNAL_ADMIN_USER_OBJ = "users"
 POLICY_SETTINGS_OBJ = "settings"
 POLICY_SENSORS_OBJ = "sensors"
-
+POLICY_LOOKER_OBJ = "looker"
 
 # warehouse
 INVALID_LAT_LONG_DIRECTION_MESSAGE = 'should be one of ("N", "S", "E", "W")'
