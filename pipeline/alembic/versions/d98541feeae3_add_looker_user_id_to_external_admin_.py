447318dbe65303df6f7b5b6e2988301ea65a45cb
karansawhney29 <karansawhney29@gmail.com>
2021-01-18 15:11:10 +0530

    Karan Sawhney: Added lookerUserID field in external admin user

---
+++ pipeline/alembic/versions/d98541feeae3_add_looker_user_id_to_external_admin_.py
@@ -0,0 +1,27 @@
+"""add_looker_user_id_to_external_admin_user
+
+Revision ID: d98541feeae3
+Revises: 7362f276939c
+Create Date: 2021-01-18 15:01:58.321488
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd98541feeae3'
+down_revision = '7362f276939c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+        op.add_column(
+        "external_admin_user", sa.Column("looker_user_id", sa.Integer(), nullable=True),
+            )
+    
+
+
+def downgrade():
+    op.drop_column("external_admin_user", "looker_user_id")
