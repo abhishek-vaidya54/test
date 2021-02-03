e2129aa24dcf72a0490245313182b4503e649225
karansawhney29 <karan.sawhney@mckinleyrice.com>
2021-02-03 17:16:30 +0530

    Karan Sawhney: Quick Import Fix

--- src/sat_orm/pipeline_orm/warehouse.py
+++ src/sat_orm/pipeline_orm/warehouse.py
@@ -34,7 +34,7 @@ from sqlalchemy import (
     event,
 )
 from sqlalchemy.orm import relationship, validates
-from sqlalchemy.dialects.mysql TIME
+from sqlalchemy.dialects.mysql import TIME
 
 # Local Application Import
 from sat_orm.pipeline_orm.pipeline_base import Base
@@ -59,7 +59,8 @@ class Warehouse(Base):
         onupdate=datetime.datetime.utcnow,
         nullable=False,
     )
-    app_restart_at = Column(TIME(),
+    app_restart_at = Column(
+        TIME(),
         nullable=True,
     )
     prefered_timezone = Column(String(100), server_default="UTC", nullable=False)
