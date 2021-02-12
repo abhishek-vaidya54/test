4dea72c29463645fb450d499d320de8949fca954
Mukul Shakya <muklshakya123@gmail.com>
2021-02-12 18:15:02 +0530

    merge alembic heads

---
+++ pipeline/alembic/versions/a6081bc75ea4_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: a6081bc75ea4
+Revises: dcfae12fc5fd, 750729396151
+Create Date: 2021-02-12 18:14:42.377173
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a6081bc75ea4'
+down_revision = ('dcfae12fc5fd', '750729396151')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
