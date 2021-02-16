b5f65792a91eb8ae872d065f451bae02b94fc95c
Joe Siefers <joe@strongarmtech.com>
2021-02-16 14:24:50 -0500

    Change base of add_show_ex_id_in_app_to_client

--- pipeline/alembic/versions/86bc75340eb7_add_show_ex_id_in_app_to_client.py
+++ pipeline/alembic/versions/86bc75340eb7_add_show_ex_id_in_app_to_client.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "86bc75340eb7"
-down_revision = "dfc30fb88fee"
+down_revision = "dcfae12fc5fd"
 branch_labels = None
 depends_on = None
 
