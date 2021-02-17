088a6be5174e02c9d87ea99175db6f5565b8a4d6
Mukul Shakya <muklshakya123@gmail.com>
2021-02-17 12:09:08 +0530

    Merged multiple heads

---
+++ pipeline/alembic/versions/4cd83b656025_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 4cd83b656025
+Revises: a2db86a00119, 86bc75340eb7
+Create Date: 2021-02-17 12:08:44.041391
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4cd83b656025'
+down_revision = ('a2db86a00119', '86bc75340eb7')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
