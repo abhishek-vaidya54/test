17be78176d51dbf485d6f1e9dc540d9aaea70c5a
mary-stewart <marystewart@strongarmtech.com>
2021-04-27 18:06:42 -0400

    WM Bedford New settings

---
+++ pipeline/alembic/versions/2af95c711733_wm_group_201_update.py
@@ -0,0 +1,43 @@
+"""WM group 201 update
+
+Revision ID: 2af95c711733
+Revises: 9eb6e8b682a1
+Create Date: 2021-04-27 18:01:39.449359
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2af95c711733'
+down_revision = '9eb6e8b682a1'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+     settings_json = """ 
+    {"handsFree":false,"eulaVersion":null,"enableMotion":true,
+    "hapticEnabled":true,"athleteEnabled":true,"showEngagement":false,
+    "enableProximity":false,
+    "showHapticModal":false,"enagementEnabled":true,
+    "hapticBendNumber":4,"enableTemperature":true,
+    "exposureRSSILimit":-48,"hapticFeedbackGap":0,
+    "showBaselineModal":false,"showSafetyJudgement":true,
+    "hapticBendPercentile":50,"hapticFeedbackWindow":600000,
+    "showSafetyScoreModal":true,"exposureHapticEnabled":false,
+    "exposureHapticRepeatMS":10000,"hapticSingleBendWindow":600,
+    "hapticSagAngleThreshold":75,"exposureHapticSuppressMS":30000}""".replace('\n', '')
+    for industrial_athlete set group_id = 206 where job_function_id in
+                    [1217, 1218, 1219, 1220, 1243]:
+        sql = """
+            insert into settings (value, target_type, target_id)
+            values ('{0}',
+                'group', {1})
+        """.format(settings_json, group)
+    op.execute(sql)
+
+
+def downgrade():
+    pass
