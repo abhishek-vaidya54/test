8404ea4b6f90d88a0d4ec1ce70eb5431a3474c44
Joe Siefers <69158324+joestrongarm@users.noreply.github.com>
2021-04-28 14:56:44 -0400

    Update settings code. (#249)

--- pipeline/alembic/versions/2af95c711733_wm_group_201_update.py
+++ pipeline/alembic/versions/2af95c711733_wm_group_201_update.py
@@ -17,19 +17,45 @@ depends_on = None
 
 
 def upgrade():
-    settings_json = """{"handsFree":false,"eulaVersion":null,"enableMotion":true,"hapticEnabled":true,"athleteEnabled":true,"showEngagement":false,"enableProximity":false,"showHapticModal":false,"enagementEnabled":true,"hapticBendNumber":4,"enableTemperature":true,"exposureRSSILimit":-48,"hapticFeedbackGap":0,"showBaselineModal":false,"showSafetyJudgement":true,"hapticBendPercentile":50,"hapticFeedbackWindow":600000,"showSafetyScoreModal":true,"exposureHapticEnabled":false,"exposureHapticRepeatMS":10000,"hapticSingleBendWindow":600,"hapticSagAngleThreshold":75,"exposureHapticSuppressMS":30000}"""
+    settings_json = """{"handsFree": false,
+        "eulaVersion": null,
+        "enableMotion": true,
+        "hapticEnabled": true,
+        "athleteEnabled": true,
+        "showEngagement": false,
+        "enableProximity": false,
+        "showHapticModal": false,
+        "enagementEnabled": true,
+        "hapticBendNumber": 4,
+        "enableTemperature": true,
+        "exposureRSSILimit": -48,
+        "hapticFeedbackGap": 0,
+        "showBaselineModal": false,
+        "showSafetyJudgement": true,
+        "hapticBendPercentile": 50,
+        "hapticFeedbackWindow": 600000,
+        "showSafetyScoreModal": true,
+        "exposureHapticEnabled": false,
+        "exposureHapticRepeatMS": 10000,
+        "hapticSingleBendWindow": 600,
+        "hapticSagAngleThreshold": 75,
+        "exposureHapticSuppressMS": 30000}""".replace('\n', '')
+
+    print(settings_json)
     sql = """insert into settings (value, target_type, target_id) values ('{0}','group', 206);""".format(settings_json)
+
+    print(sql)
     op.execute(sql)
 
     op.execute(
         """
-        UPDATE industrial_athlete SET group_id=206 WHERE job_function_id in (1217, 1218, 1219, 1220, 1243);
+            UPDATE industrial_athlete SET group_id=206 WHERE job_function_id in (1217, 1218, 1219, 1220, 1243);
         """  
     )
 
     op.execute(
         """
-        UPDATE groups SET override_settings=1 WHERE id=206;
+            UPDATE groups SET override_settings=1 WHERE id=206;
         """  
     )
 
