bf285b16a81a6591c22e25eb5ef6595119b6c80a
mary-stewart <marystewart@strongarmtech.com>
2021-05-11 16:40:51 -0400

    spaces after colon

--- pipeline/alembic/versions/dca6817709bb_summerville_rssi_update.py
+++ pipeline/alembic/versions/dca6817709bb_summerville_rssi_update.py
@@ -17,7 +17,7 @@ depends_on = None
 
 
 def upgrade():
-    settings_json = """{"handsFree":true,"eulaVersion":null,"enableMotion":false,"hapticEnabled":false,"athleteEnabled":true,"showEngagement":false,"enableProximity":true,"showHapticModal":false,"enagementEnabled":false,"hapticBendNumber":0,"enableTemperature":false,"exposureRSSILimit":-58,"hapticFeedbackGap":0,"showBaselineModal":false,"showSafetyJudgement":true,"hapticBendPercentile":0,"hapticFeedbackWindow":0,"showSafetyScoreModal":false,"exposureHapticEnabled":true,"exposureHapticRepeatMS":10000,"hapticSingleBendWindow":0,"hapticSagAngleThreshold":0,"exposureHapticSuppressMS":30000}""".replace('\n', '')
+    settings_json = """{"handsFree": true,"eulaVersion": null,"enableMotion": false,"hapticEnabled": false,"athleteEnabled": true,"showEngagement": false,"enableProximity": true,"showHapticModal": false,"enagementEnabled": false,"hapticBendNumber": 0,"enableTemperature": false,"exposureRSSILimit": -58,"hapticFeedbackGap": 0,"showBaselineModal": false,"showSafetyJudgement": true,"hapticBendPercentile": 0,"hapticFeedbackWindow": 0,"showSafetyScoreModal": false,"exposureHapticEnabled": true,"exposureHapticRepeatMS": 10000,"hapticSingleBendWindow": 0,"hapticSagAngleThreshold": 0,"exposureHapticSuppressMS": 30000}""".replace('\n', '')
 
     for warehouse in [172]:
         sql = """
