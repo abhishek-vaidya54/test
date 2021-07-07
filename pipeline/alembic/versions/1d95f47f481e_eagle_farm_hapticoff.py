fcd5bc268365d1a6fc571c438f0884c12685fc1a
mary-stewart <marystewart@strongarmtech.com>
2021-07-07 11:47:35 -0400

    eagle_farm_haptic_off

---
+++ pipeline/alembic/versions/1d95f47f481e_eagle_farm_hapticoff.py
@@ -0,0 +1,54 @@
+"""eagle_farm_hapticoff
+
+Revision ID: 1d95f47f481e
+Revises: 965450c70097
+Create Date: 2021-07-07 11:40:30.911989
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1d95f47f481e'
+down_revision = '965450c70097'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    baseline_settings  = """{ "handsFree": false,
+                "eulaVersion": null,
+                "enableMotion": true,
+                "hapticEnabled": false,
+                "athleteEnabled": false,
+                "showEngagement": true,
+                "enableProximity": false,
+                "showHapticModal": false,
+                "enagementEnabled": true,
+                "hapticBendNumber": 10,
+                "enableTemperature": true,
+                "exposureRSSILimit": -48,
+                "hapticFeedbackGap": 0,
+                "showBaselineModal": true,
+                "showSafetyJudgement": true,
+                "hapticBendPercentile": 50,
+                "hapticFeedbackWindow": 0,
+                "showSafetyScoreModal": true,
+                "exposureHapticEnabled": false,
+                "exposureHapticRepeatMS": 10000,
+                "hapticSingleBendWindow": 600,
+                "hapticSagAngleThreshold": 60,
+                "exposureHapticSuppressMS": 30000}""".replace('\n', '')
+
+    for warehouse in [228]:
+        sql = """
+            insert into settings (value, target_type, target_id)
+            values ('{0}',
+                'warehouse', {1})
+        """.format(baseline_settings, warehouse)
+        op.execute(sql)
+
+
+def downgrade():
+    pass
