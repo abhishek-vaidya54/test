02c75fa5da380d60a1eff9ef4cbd81b6917292c2
Karan Sawhney <karansawhney29@gmail.com>
2021-07-08 15:48:47 +0530

    Update 6babc516b4da_replace_is_active_column_to_account_.py

--- pipeline/alembic/versions/6babc516b4da_replace_is_active_column_to_account_.py
+++ pipeline/alembic/versions/6babc516b4da_replace_is_active_column_to_account_.py
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '6babc516b4da'
-down_revision = 'f5641c61905a'
+revision = "6babc516b4da"
+down_revision = "f5641c61905a"
 branch_labels = None
 depends_on = None
 
@@ -19,13 +19,21 @@ depends_on = None
 def upgrade():
     op.drop_column("external_admin_user", "is_active")
     op.add_column(
-        "external_admin_user", sa.Column("account_status", sa.String(length=255), nullable=False, server_default='inactive')
+        "external_admin_user",
+        sa.Column(
+            "account_status",
+            sa.String(length=255),
+            nullable=False,
+            server_default="inactive",
+        ),
     )
 
 
 def downgrade():
     op.drop_column("external_admin_user", "account_status")
     op.add_column(
-        "external_admin_user", sa.Column("is_active", sa.String(length=5), nullable=False, server_default='true')
+        "external_admin_user",
+        sa.Column(
+            "is_active", sa.String(length=5), nullable=False, server_default=True
+        ),
     )
-    
