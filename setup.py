5caec72bcc3c3201ef133b8b55bd527ab3475583
NorbertoStudios <norbertostudios@gmail.com>
2021-07-09 11:45:24 -0400

    fixing sqlalchemy lock version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages("src"),
     package_dir={"": "src"},
     install_requires=[
-        "sqlalchemy==1.4.20",
+        "sqlalchemy==1.4.19",
         "pymysql==1.0.2",
         "pytest==6.2.4",
         "factory-boy==3.2.0",
