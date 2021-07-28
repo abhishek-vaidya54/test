63c4b7bb5c00b1fe3e9361c0236543b21d863ab2
Norberto Fernandez <34346031+NorbertoStudios@users.noreply.github.com>
2021-07-28 12:15:32 -0400

    Update 826a1788bc80_newlab_haptic_off.py

--- pipeline/alembic/versions/826a1788bc80_newlab_haptic_off.py
+++ pipeline/alembic/versions/826a1788bc80_newlab_haptic_off.py
@@ -41,13 +41,11 @@ def upgrade():
                 "hapticSagAngleThreshold": 70,
                 "exposureHapticSuppressMS": 30000}""".replace('\n', '')
 
-    for warehouse in [77]:
+     for warehouse in [77]:
         sql = """
-            insert into settings (value, target_type, target_id)
-            values ('{0}',
-                'warehouse', {1})
-        """.format(     json_settings = """{ "handsFree": false,
-, warehouse)
+          insert into settings (value, target_type, target_id) 
+          values ('{0}', 'warehouse', {1})
+        """.format(json_settings = """{ "handsFree": false, warehouse)
         op.execute(sql)
 
 
