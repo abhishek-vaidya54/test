fcb89638a414b96f8b781f8dd8d903eea475e173
Medwin <medwin@strongarmtech.com>
2021-08-25 10:25:47 -0400

    crestmead 2.0 haptic parameters

---
+++ pipeline/alembic/versions/faa7947d2dfa_crestmead2.py
@@ -0,0 +1,54 @@
+"""crestmead2
+
+Revision ID: faa7947d2dfa
+Revises: bc2e67312626
+Create Date: 2021-08-25 10:12:17.426046
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'faa7947d2dfa'
+down_revision = 'bc2e67312626'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    warehouse_settings_json = """{ "handsFree": false,
+            "eulaVersion": null,
+            "enableMotion": true,
+            "hapticEnabled": false,
+            "athleteEnabled": false,
+            "showEngagement": true,
+            "enableProximity": false,
+            "showHapticModal": false,
+            "enagementEnabled": false,
+            "hapticBendNumber": 1,
+            "enableTemperature": true,
+            "exposureRSSILimit": -48,
+            "hapticFeedbackGap": 0,
+            "showBaselineModal": true,
+            "showSafetyJudgement": true,
+            "hapticBendPercentile": 50,
+            "hapticFeedbackWindow": 0,
+            "showSafetyScoreModal": true,
+            "exposureHapticEnabled": true,
+            "exposureHapticRepeatMS": 10000,
+            "hapticSingleBendWindow": 600,
+            "hapticSagAngleThreshold": 70,
+            "exposureHapticSuppressMS": 30000}""".replace('\n', '')
+
+    for warehouse in [332]:
+        sql = """
+            insert into settings (value, target_type, target_id)
+            values ('{0}',
+                'warehouse', {1})
+        """.format(warehouse_settings_json, warehouse)
+        op.execute(sql)
+
+
+def downgrade():
+    pass
