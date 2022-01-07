50775e39849acc26d435754f038600ded67360c6
Karan Sawhney <karansawhney29@gmail.com>
2022-01-07 11:09:52 +0530

    comment firmware and firmware group

--- src/sat_orm/dockv5_orm/firmware.py
+++ src/sat_orm/dockv5_orm/firmware.py
@@ -14,10 +14,12 @@ class Firmware(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     version = Column(String(255), nullable=False)
     s3_url = Column(String(255), nullable=True)
-    device_type_id = Column(Integer, ForeignKey("device_type.id"), nullable=False)
+    device_type_id = Column(Integer, ForeignKey(
+        "device_type.id"), nullable=False)
     hardware_id = Column(Integer, ForeignKey("hardware.id"), nullable=False)
 
-    db_created_at = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
+    db_created_at = Column(
+        DateTime, default=datetime.datetime.utcnow, nullable=False)
 
     # Relationships
     hardware = relationship("Hardware", back_populates="firmwares")
@@ -31,89 +33,89 @@ class Firmware(Base):
         }
 
 
-@event.listens_for(Firmware, "before_insert")
-def validate_before_insert(mapper, connection, target):
-    """
-    Helper method to check if params are valid for adding a single firmware
-    Input:
-        params_input: json containing data to be added for a single firmware.
-    """
-    params_input = {}
-    for key, value in target.as_dict().items():
-        if value is not None:
-            params_input[key] = value
-    errors = []
+# @event.listens_for(Firmware, "before_insert")
+# def validate_before_insert(mapper, connection, target):
+#     """
+#     Helper method to check if params are valid for adding a single firmware
+#     Input:
+#         params_input: json containing data to be added for a single firmware.
+#     """
+#     params_input = {}
+#     for key, value in target.as_dict().items():
+#         if value is not None:
+#             params_input[key] = value
+#     errors = []
 
-    version = params_input.get("version", "")
-    s3_url = params_input.get("s3_url", "")
-    device_type_id = params_input.get("device_type_id")
-    hardware_id = params_input.get("hardware_id")
+#     version = params_input.get("version", "")
+#     s3_url = params_input.get("s3_url", "")
+#     device_type_id = params_input.get("device_type_id")
+#     hardware_id = params_input.get("hardware_id")
 
-    is_valid = fu.is_non_empty_string(version)
-    if not is_valid:
-        errors.append(build_error("version", constants.EMPTY_STRING_ERROR_MESSAGE))
+#     is_valid = fu.is_non_empty_string(version)
+#     if not is_valid:
+#         errors.append(build_error("version", constants.EMPTY_STRING_ERROR_MESSAGE))
 
-    is_valid = fu.is_non_empty_string(s3_url)
-    if not is_valid:
-        errors.append(build_error("s3_url", constants.EMPTY_STRING_ERROR_MESSAGE))
+#     is_valid = fu.is_non_empty_string(s3_url)
+#     if not is_valid:
+#         errors.append(build_error("s3_url", constants.EMPTY_STRING_ERROR_MESSAGE))
 
-    is_valid = fu.is_valid_device_type_id(connection, device_type_id)
-    if not is_valid:
-        errors.append(
-            build_error("device_type_id", constants.INVALID_DEVICE_TYPE_MESSAGE)
-        )
+#     is_valid = fu.is_valid_device_type_id(connection, device_type_id)
+#     if not is_valid:
+#         errors.append(
+#             build_error("device_type_id", constants.INVALID_DEVICE_TYPE_MESSAGE)
+#         )
 
-    is_valid = fu.is_valid_hardware_id(connection, hardware_id)
-    if not is_valid:
-        errors.append(build_error("hardware_id", constants.INVALID_HARDWARE_MESSAGE))
+#     is_valid = fu.is_valid_hardware_id(connection, hardware_id)
+#     if not is_valid:
+#         errors.append(build_error("hardware_id", constants.INVALID_HARDWARE_MESSAGE))
 
-    check_errors_and_return(errors)
+#     check_errors_and_return(errors)
 
 
-@event.listens_for(Firmware, "before_update")
-def validate_before_update(mapper, connection, target):
-    """
-    Helper method to check if params are valid for adding a single athlete
-    Input:
-        params_input: json containing data to be added for a single athlete.
-        warehouse_id: warehouse ID to validate job function
-    """
-    params_input = {}
-    for key, value in target.as_dict().items():
-        if value is not None:
-            params_input[key] = value
-    errors = []
+# @event.listens_for(Firmware, "before_update")
+# def validate_before_update(mapper, connection, target):
+#     """
+#     Helper method to check if params are valid for adding a single athlete
+#     Input:
+#         params_input: json containing data to be added for a single athlete.
+#         warehouse_id: warehouse ID to validate job function
+#     """
+#     params_input = {}
+#     for key, value in target.as_dict().items():
+#         if value is not None:
+#             params_input[key] = value
+#     errors = []
 
-    if "id" in params_input:
-        is_valid = fq.get_firmware_exists(connection, params_input.get("id", ""))
-        if not is_valid:
-            errors.append(build_error("id", constants.INVALID_FIRMWARE_ID))
+#     if "id" in params_input:
+#         is_valid = fq.get_firmware_exists(connection, params_input.get("id", ""))
+#         if not is_valid:
+#             errors.append(build_error("id", constants.INVALID_FIRMWARE_ID))
 
-    if "version" in params_input:
-        is_valid = fu.is_non_empty_string(params_input.get("version", ""))
-        if not is_valid:
-            errors.append(build_error("version", constants.EMPTY_STRING_ERROR_MESSAGE))
+#     if "version" in params_input:
+#         is_valid = fu.is_non_empty_string(params_input.get("version", ""))
+#         if not is_valid:
+#             errors.append(build_error("version", constants.EMPTY_STRING_ERROR_MESSAGE))
 
-    if "s3_url" in params_input:
-        is_valid = fu.is_non_empty_string(params_input.get("s3_url", ""))
-        if not is_valid:
-            errors.append(build_error("s3_url", constants.EMPTY_STRING_ERROR_MESSAGE))
+#     if "s3_url" in params_input:
+#         is_valid = fu.is_non_empty_string(params_input.get("s3_url", ""))
+#         if not is_valid:
+#             errors.append(build_error("s3_url", constants.EMPTY_STRING_ERROR_MESSAGE))
 
-    if "device_type_id" in params_input:
-        is_valid = fu.is_valid_device_type_id(
-            connection, params_input.get("device_type_id", "")
-        )
-        if not is_valid:
-            errors.append(
-                build_error("device_type_id", constants.INVALID_DEVICE_TYPE_MESSAGE)
-            )
+#     if "device_type_id" in params_input:
+#         is_valid = fu.is_valid_device_type_id(
+#             connection, params_input.get("device_type_id", "")
+#         )
+#         if not is_valid:
+#             errors.append(
+#                 build_error("device_type_id", constants.INVALID_DEVICE_TYPE_MESSAGE)
+#             )
 
-    if "hardware_id" in params_input:
-        is_valid = fu.is_valid_hardware_id(params_input.get("hardware_id", ""))
-        if not is_valid:
-            errors.append(
-                build_error("hardware_id", constants.INVALID_HARDWARE_MESSAGE)
-            )
+#     if "hardware_id" in params_input:
+#         is_valid = fu.is_valid_hardware_id(params_input.get("hardware_id", ""))
+#         if not is_valid:
+#             errors.append(
+#                 build_error("hardware_id", constants.INVALID_HARDWARE_MESSAGE)
+#             )
 
-    check_errors_and_return(errors)
-    pass
+#     check_errors_and_return(errors)
+#     pass
--- src/sat_orm/dockv5_orm/firmware_group.py
+++ src/sat_orm/dockv5_orm/firmware_group.py
@@ -14,10 +14,12 @@ class FirmwareGroup(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(255), nullable=False)
     description = Column(String(255), nullable=False)
-    db_created_at = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
+    db_created_at = Column(
+        DateTime, default=datetime.datetime.utcnow, nullable=False)
 
     # Relationships
-    firmwares = relationship(FirmwareGroupAssociation, back_populates=__tablename__)
+    firmwares = relationship(FirmwareGroupAssociation,
+                             back_populates=__tablename__)
 
     configs = relationship("Config", back_populates="firmware_group")
 
@@ -29,58 +31,58 @@ class FirmwareGroup(Base):
         }
 
 
-@event.listens_for(FirmwareGroup, "before_insert")
-def validate_before_insert(mapper, target):
-    """
-    Helper method to check if params are valid for adding a single firmware
-    Input:
-        params_input: json containing data to be added for a single firmware.
-    """
-    params_input = {}
-    for key, value in target.as_dict().items():
-        if value is not None:
-            params_input[key] = value
-    errors = []
+# @event.listens_for(FirmwareGroup, "before_insert")
+# def validate_before_insert(mapper, target):
+#     """
+#     Helper method to check if params are valid for adding a single firmware
+#     Input:
+#         params_input: json containing data to be added for a single firmware.
+#     """
+#     params_input = {}
+#     for key, value in target.as_dict().items():
+#         if value is not None:
+#             params_input[key] = value
+#     errors = []
 
-    name = params_input.get("name", "")
-    description = params_input.get("description", "")
+#     name = params_input.get("name", "")
+#     description = params_input.get("description", "")
 
-    is_valid = fu.is_non_empty_string(name)
-    if not is_valid:
-        errors.append(build_error("name", constants.EMPTY_STRING_ERROR_MESSAGE))
+#     is_valid = fu.is_non_empty_string(name)
+#     if not is_valid:
+#         errors.append(build_error("name", constants.EMPTY_STRING_ERROR_MESSAGE))
 
-    is_valid = fu.is_non_empty_string(description)
-    if not is_valid:
-        errors.append(build_error("description", constants.EMPTY_STRING_ERROR_MESSAGE))
+#     is_valid = fu.is_non_empty_string(description)
+#     if not is_valid:
+#         errors.append(build_error("description", constants.EMPTY_STRING_ERROR_MESSAGE))
 
-    check_errors_and_return(errors)
+#     check_errors_and_return(errors)
 
 
-@event.listens_for(FirmwareGroup, "before_update")
-def validate_before_update(mapper, target):
-    """
-    Helper method to check if params are valid for adding a single athlete
-    Input:
-        params_input: json containing data to be added for a single athlete.
-        warehouse_id: warehouse ID to validate job function
-    """
-    params_input = {}
-    for key, value in target.as_dict().items():
-        if value is not None:
-            params_input[key] = value
-    errors = []
+# @event.listens_for(FirmwareGroup, "before_update")
+# def validate_before_update(mapper, target):
+#     """
+#     Helper method to check if params are valid for adding a single athlete
+#     Input:
+#         params_input: json containing data to be added for a single athlete.
+#         warehouse_id: warehouse ID to validate job function
+#     """
+#     params_input = {}
+#     for key, value in target.as_dict().items():
+#         if value is not None:
+#             params_input[key] = value
+#     errors = []
 
-    if "name" in params_input:
-        is_valid = fu.is_non_empty_string(params_input.get("name", ""))
-        if not is_valid:
-            errors.append(build_error("name", constants.EMPTY_STRING_ERROR_MESSAGE))
+#     if "name" in params_input:
+#         is_valid = fu.is_non_empty_string(params_input.get("name", ""))
+#         if not is_valid:
+#             errors.append(build_error("name", constants.EMPTY_STRING_ERROR_MESSAGE))
 
-    if "description" in params_input:
-        is_valid = fu.is_non_empty_string(params_input.get("description", ""))
-        if not is_valid:
-            errors.append(
-                build_error("description", constants.EMPTY_STRING_ERROR_MESSAGE)
-            )
+#     if "description" in params_input:
+#         is_valid = fu.is_non_empty_string(params_input.get("description", ""))
+#         if not is_valid:
+#             errors.append(
+#                 build_error("description", constants.EMPTY_STRING_ERROR_MESSAGE)
+#             )
 
-    check_errors_and_return(errors)
-    pass
+#     check_errors_and_return(errors)
+#     pass
