2d351f77f75670a3aa3dc4513c7af0a707148105
Karan Sawhney <karansawhney29@gmail.com>
2022-09-12 16:07:02 +0530

    Email Scheduling Token Table

---
+++ pipeline/alembic/versions/510598f20cf5_create_email_scheduling_token_table.py
@@ -0,0 +1,41 @@
+"""create_email_scheduling_token_table
+
+Revision ID: 510598f20cf5
+Revises: 8c094ff04dcd
+Create Date: 2022-09-12 15:04:50.538819
+
+"""
+from alembic import op
+import sqlalchemy as sa
+import datetime, uuid
+
+# revision identifiers, used by Alembic.
+revision = "510598f20cf5"
+down_revision = "8c094ff04dcd"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        "email_scheduling_token",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("report_id", sa.Integer(), nullable=False),
+        sa.Column("token_id", sa.UnicodeText(), nullable=False, default=uuid.uuid4),
+        sa.Column(
+            "db_created_at",
+            sa.DateTime,
+            default=datetime.datetime.utcnow,
+            nullable=False,
+        ),
+        sa.PrimaryKeyConstraint("id"),
+        sa.ForeignKeyConstraint(
+            ["report_id"],
+            ["report_subscribe.id"],
+            name="fk_report_subscribe_email_scheduling_token",
+        ),
+    )
+
+
+def downgrade():
+    op.drop_table("email_scheduling_token")
