86246bd9278feea411c455ac5fc70e7cd02cfdfc
samuelopa <100146814+samuelopa@users.noreply.github.com>
2022-11-04 11:51:31 -0400

    merged two heads

---
+++ pipeline/alembic/versions/6bdad663e8ef_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 6bdad663e8ef
+Revises: 2f8b8f37c79a, 0b49da425038
+Create Date: 2022-11-04 11:50:50.236968
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6bdad663e8ef'
+down_revision = ('2f8b8f37c79a', '0b49da425038')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
