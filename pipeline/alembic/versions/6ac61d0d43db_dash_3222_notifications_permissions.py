ad68399a6a991272bd1ac2758e44a186acd133a2
varunallasat <106978743+varunallasat@users.noreply.github.com>
2022-11-14 06:13:12 -0800

    DASH-3222 Users who don't have access to the Industrial Athlete Page can't see notifications (#551)
    * DASH-3222 notification mappings for intervention page and ergonomic looker
    
    * Fix code style issues with Black
    
    * updated the migration script to apply notificaitons to users with any role and downgrade to remove gets except superuser
    
    * Fix code style issues with Black
    
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>

---
+++ pipeline/alembic/versions/6ac61d0d43db_dash_3222_notifications_permissions.py
@@ -0,0 +1,52 @@
+"""dash_3222_notifications_permissions
+
+Revision ID: 6ac61d0d43db
+Revises: 8be9b364cbda
+Create Date: 2022-10-26 08:15:13.938466
+
+"""
+from alembic import op
+from sqlalchemy import orm
+
+
+# revision identifiers, used by Alembic.
+revision = "6ac61d0d43db"
+down_revision = "8be9b364cbda"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    bind = op.get_bind()
+    session = orm.Session(bind=bind)
+    session.execute(
+        """
+        DELETE FROM pipeline.casbin_rule
+        WHERE v1 = 'notifications' AND v2 = 'get' AND ptype='p';
+    """
+    )
+    session.execute(
+        """
+        INSERT INTO pipeline.casbin_rule (ptype, v0, v1, v2)
+	    VALUES  ('p', 'looker_ergo', 'notifications', 'get'),
+                ('p', 'superuser', 'notifications', 'get'),
+                ('p', 'shift_manager', 'notifications', 'get'),
+                ('p', 'admin', 'notifications', 'get'),
+                ('p', 'manager', 'notifications', 'get'),
+                ('p', 'looker_prox', 'notifications', 'get'),
+                ('p', 'bulk_upload', 'notifications', 'get'),
+                ('p', 'analytics_heatstress', 'notifications', 'get');
+        """
+    )
+    session.commit()
+    session.close()
+
+
+def downgrade():
+    op.execute(
+        """
+        DELETE FROM pipeline.casbin_rule
+        WHERE v0<>'superuser' AND  v1 = 'notifications' 
+                              AND v2 = 'get' AND ptype='p';
+        """
+    )
